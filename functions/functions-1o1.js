769e9d119b04036c3f53c3aac55a78592610502c
Carl Woolley <info@carlwoolley.co.nz>
2018-07-12 17:40:21 +1200

    updating course files

---
+++ functions/functions-1o1.js
@@ -0,0 +1,20 @@
+//function - input, code, output
+
+
+let greetUser = function(){
+    console.log('hello');
+}
+
+greetUser();
+
+let square = function(num){
+
+    let result = num * num;
+    return result;
+}
+
+let value = square(3);
+let valueTwo = square(10);
+
+console.log(value);
+console.log(valueTwo);
